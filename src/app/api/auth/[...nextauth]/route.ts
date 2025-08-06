dedde037fcf15d88c25b5a7f7af16100fb24d5a2
wm0x <aloh200200@gmail.com>
2025-08-06 10:29:27 +0300

    fix problem: ready for deploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,2 @@
-// src/app/api/auth/[...nextauth]/route.ts
 import { handlers } from "@/auth";
-import { NextResponse } from "next/server";
-
-export const dynamic = 'force-dynamic'; // Add this line
-export const runtime = 'nodejs'; // Optional but recommended
-
 export const { GET, POST } = handlers;
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -74,7 +74,7 @@ export const SidebarBody = (props: React.ComponentProps<typeof motion.div>) => {
   return (
     <>
       <DesktopSidebar {...props} />
-      <MobileSidebar {...(props as React.ComponentProps<"div">)} />
+      <MobileSidebar {...(props as unknown as React.ComponentProps<"div">)} />
     </>
   );
 };
